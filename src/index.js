64cb24d8c39aebbcb1955d302f3e3c35ebadbb58
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2019-03-19 16:01:19 +0200

    added default exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const EventSourceBase = require('../src/dynamodb/EventSourceBase');
+const StreamListener = require('../src/dynamodb/StreamListener');
+
+module.exports = {
+  EventSourceBase,
+  StreamListener
+};
