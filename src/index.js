b7f986059d9cf51ae365ee85f316ab9b6a4f0db1
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2019-03-19 16:05:15 +0200

    moved index to be at the correct path

--- src/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const EventSourceBase = require('../src/dynamodb/EventSourceBase');
-const StreamListener = require('../src/dynamodb/StreamListener');
-
-module.exports = {
-  EventSourceBase,
-  StreamListener
-};
